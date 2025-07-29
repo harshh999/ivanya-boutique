9d0992facfab98f517f9393ef5540831913e655b
harshh999 <harshgohil.biz@gmail.com>
2025-07-29 21:13:39 +0530

    Build complete website with all features

---
+++ src/data/products.js
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    id: 'iv-001',
+    name: 'Gulabi Kurti',
+    price: '₹699',
+    image: '/images/gulabi-kurti.png', // <-- FIXED!
+    featured: true,
+  },
+  {
+    id: 'iv-002',
+    name: 'Bebo Kurti',
+    price: '₹699',
+    image: '/images/bebo-kurti.png',   // <-- FIXED!
+    featured: true,
+  },
+  {
+    id: 'iv-003',
+    name: 'Gulnaar Kurti',
+    price: '₹699',
+    image: '/images/gulnaar-kurti.png', // <-- FIXED!
+    featured: true,
+  },
+  // You can add more products here later!
+];
